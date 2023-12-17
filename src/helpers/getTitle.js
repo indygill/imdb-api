a9f49bd416dd338098ef22c699e2e418d8a6693b
Indy Gill <46026061+indygill@users.noreply.github.com>
2023-12-17 14:54:39 +0000

    Update getTitle.js
    Added prop check for originLocations.

--- src/helpers/getTitle.js
+++ src/helpers/getTitle.js
@@ -48,7 +48,7 @@ export default async function getTitle(id) {
         },
         originLocations: releaseDateMeta.countriesOfOrigin.countries.map(
           (e) => ({
-            country: e.text,
+            country: e?.text ?? "",
             cca2: e.id,
           })
         ),
